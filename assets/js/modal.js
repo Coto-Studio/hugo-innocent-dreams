5e2cfd13498efdd410f9ae0f6e130c5244e69f42
Nelson de Witt <git@nelsonroberto.com>
2022-12-22 10:43:22 -0500

    Modal JS only runs when modal is found

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -2,10 +2,12 @@ const btnOpenModal = document.querySelector(".open-modal");
 const btnClose = document.querySelector(".close-modal");
 const modal = document.querySelector(".notify-modal");
 
-btnOpenModal.addEventListener("click", () => {
-  modal.showModal();
-});
+if (modal != undefined) {
+  btnOpenModal.addEventListener("click", () => {
+    modal.showModal();
+  });
 
-btnClose.addEventListener("click", () => {
-  modal.close();
-});
+  btnClose.addEventListener("click", () => {
+    modal.close();
+  });
+}
